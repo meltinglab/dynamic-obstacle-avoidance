f384444272a92c763da500806735e2cdbf5697a4
Ornitorinco27 <72251993+Ornitorinco27@users.noreply.github.com>
2021-05-11 11:23:54 +0200

    Update Obstacle avoidance function
    Added 2 new zones and some adjustments on the algorithm

--- Code/Scripts/LaneKeepConstraint.m
+++ Code/Scripts/LaneKeepConstraint.m
@@ -1,49 +1,87 @@
-function [E, F, G] = LaneKeepConstraint(Reference,Lw,detection,x,SafeX,SafeY,EndX,EndY,EntryPoint,Zone,DetectionPoint)
+function [E, F, G] = LaneKeepConstraint(Reference,Lw,detection,SafeX,SafeY,EndX,EndY,EntryPoint,Zone,DetectionPoint)
+% LaneKeepConstraint function generates the matrices E F G to give to the
+% controller in order to define linear constraint.
+%
+% Controller needs to satisfy the following equation:
+%           E*u(k+j)+F*y(k+j)<=G
+%
+% LaneKeepConstraint take as input:
+% - Reference -         Actual reference point for the controller
+% - Lw -                [m] Lane Width
+% - detection -     flag that is 1 when obstacle is detected, 0 otherwise
+% - SafeX,SafeY -   Point where starts the Safe Zone, as lower bound of the
+%                   overtaking (left) lane
+% - EndX,EndY -     Point where ends the Safe Zone, as lower bound of the
+%                   overtaking (left) lane
+% - EntryPoint -    Point in the reference map, after the obstacle, where
+%                   the vehicle should return after the overtaking maneuver
+% - Zone -          flag that tell where the vehicle is with respect to the
+%                   obstacle. Outcomes are:
+%                       - 0 - No obstacle
+%                       - 1 - Well before the obstacle
+%                       - 2 - Before the obstacle, leave the lane
+%                       - 3 - In the Safe Zone, stay in the left lane
+%                       - 4 - After the Safe Zone, come back in your lane
+%                       - 5 - Obstacle passed 
+% - DetectionPoint -    Point where to start leave the lane to go in the
+%                       Safe Zone
+%
 
+
+
+    % Acquire the Reference point
     X_ref = Reference(1);
     Y_ref = Reference(2);
     Theta_ref = Reference(3);
-       
+    
+    % Normalize Theta_ref to -pi:pi
+    Theta_ref = Theta_ref + pi;
+    Theta_ref = mod(Theta_ref,2*pi);
+    Theta_ref = Theta_ref - pi;
+    
+    % Set 0 constraints on input
     E = [0 0
         0 0];
     
-    X_act = x(1);
-    Y_act = x(2);
-    
+    % Point where to reenter in the lane when obstacle is passed
     X_entry = EntryPoint(1);
     Y_entry = EntryPoint(2);
     
+    % Point where to leave tha lane when obstacle is detected
     X_det = DetectionPoint(1);
     Y_det = DetectionPoint(2);
 
-
+    % Road orientation
     m = tan(Theta_ref);
-    if abs(m) > 1
+    
+    if abs(m) > 1 % Close to vertical
         
-        m = cot(Theta_ref);
+        m = cot(Theta_ref); % Inverse angular coefficient
         
+        % Lane boundaries
         q1 = X_ref + 1/2*Lw/sin(Theta_ref) - m*Y_ref;        
         q2 = X_ref - 1/2*Lw/sin(Theta_ref) - m*Y_ref;
 
 
         if detection
-            q2 = q2 - Lw/sin(Theta_ref);
-            if Zone == 1 % Before the Safe
+            q2 = q2 - Lw/sin(Theta_ref); % Bound of the overtaking lane
+            
+            if Zone == 2 % Before the Safe, change line
                 slope = ( (SafeX - X_det)/(SafeY - Y_det) );
                 constraintSlope = slope;
                 constraintIntercept = SafeX - constraintSlope*SafeY;
 
-            elseif Zone == 2 % In the Safe Zone
-%                 slope = ( (EndX - SafeX)/(EndY - SafeY) );
-%                 constraintSlope = slope;
-%                 constraintIntercept = EndX - constraintSlope*EndY;
+            elseif Zone == 3 % In the Safe Zone
                 slope = m;
                 constraintSlope = slope;
                 constraintIntercept = X_ref - Lw/sin(Theta_ref) - m*Y_ref;
-            else % After the Safe
+            elseif Zone == 4 % After the Safe
                 entrySlope = ((X_entry - EndX)/(Y_entry-EndY));
                 constraintSlope = entrySlope;
                 constraintIntercept = X_entry - constraintSlope*(Y_entry);
+            else %Far from the obstacle
+                constraintSlope = m;
+                constraintIntercept = q1;                
             end
 
             if Theta_ref > 0 && constraintSlope <= 0
@@ -54,7 +92,7 @@ function [E, F, G] = LaneKeepConstraint(Reference,Lw,detection,x,SafeX,SafeY,End
 
                 G = [constraintIntercept; -q2];
             elseif Theta_ref < 0 && constraintSlope > 0
-                % x < constraintIntercept + constraintSlope*Y
+                % x > constraintIntercept + constraintSlope*Y
                 % x < q2 + m*Y
                 F = [-1 constraintSlope 0 0
                     1 -m 0 0];
@@ -68,21 +106,22 @@ function [E, F, G] = LaneKeepConstraint(Reference,Lw,detection,x,SafeX,SafeY,End
                     -1 m 0 0];
                 G = [constraintIntercept; -q2];
             else
-
+                 % x > constraintIntercept + constraintSlope*Y
+                 % x < q2 + m*Y
                 F = [-1 constraintSlope 0 0
                      1 -m 0 0];
                 G = [-constraintIntercept; q2];
 
             end
 
-        else
+        else % No obstacle
 %             if Theta_ref < 0
-%                 F = [1 -m 0 0
-%                     -1 m 0 0];
-%                 G = [-q1; q2];
-%             else
 %                 F = [-1 m 0 0
 %                     1 -m 0 0];
+%                 G = [-q1; q2];
+%             else
+%                 F = [1 -m 0 0
+%                     -1 m 0 0];
 %                 G = [q1; -q2];
 %             end
             F = [0 0 0 0
@@ -99,7 +138,7 @@ function [E, F, G] = LaneKeepConstraint(Reference,Lw,detection,x,SafeX,SafeY,End
         
         
         
-    else
+    else % close to horizontal
     
         q1 = Y_ref - 1/2*Lw/cos(Theta_ref) - m*X_ref;        
         q2 = Y_ref + 1/2*Lw/cos(Theta_ref) - m*X_ref;
@@ -107,22 +146,25 @@ function [E, F, G] = LaneKeepConstraint(Reference,Lw,detection,x,SafeX,SafeY,End
 
         if detection
             q2 = q2 + Lw/cos(Theta_ref);
-            if Zone == 1 % Before the Safe
+            if Zone == 2 % Before the Safe
                 slope = ( (SafeY - Y_det)/(SafeX - X_det) );
                 constraintSlope = slope;
                 constraintIntercept = SafeY - constraintSlope*SafeX;
 
-            elseif Zone == 2 % In the Safe Zone
+            elseif Zone == 3 % In the Safe Zone
 %                 slope = ( (EndY - SafeY)/(EndX - SafeX) );
 %                 constraintSlope = slope;
 %                 constraintIntercept = EndY - constraintSlope*EndX;
                 slope = m;
                 constraintSlope = slope;
                 constraintIntercept = Y_ref + Lw/cos(Theta_ref) - m*X_ref;
-            else % After the Safe
+            elseif Zone == 4 % After the Safe
                 entrySlope = ((Y_entry - EndY)/(X_entry-EndX));
                 constraintSlope = entrySlope;
                 constraintIntercept = Y_entry - constraintSlope*(X_entry);
+            else
+                constraintSlope = m;
+                constraintIntercept = q1;
             end
 
             if abs(Theta_ref) <= pi/2 && constraintSlope >= 0
--- Code/Scripts/detectionFun.m
+++ Code/Scripts/detectionFun.m
@@ -1,5 +1,38 @@
 function [SafeX,SafeY,EndX,EndY,EntryPoint,detection,Zone,indexObs_old,DetectionPoint] = detectionFun(Actual,Obstacle,map,Lw,Ts,indexObs)
-    
+% This funtion takes as input the position of the obstacle, the actual
+% position of the vehicle, the reference map and the Lanewidth to give as
+% output a detection flag and a set of points to define a safety zone
+% around the obstacle.
+% Inputs are:
+%   - Actual -      actual state (x,y,theta,v) of the vehicle
+%   - Obstacle -    position (x,y) of the obstacle
+%   - map -         reference map NSteps*4
+%   - Lw -          [m] Lane width
+%   - Ts -          [s] Time step 
+%   - indexObs -    Variable used to avoid to search obstacle on map at
+%                   every cycle
+%
+% Outputs are:
+% - SafeX,SafeY -   Point where starts the Safe Zone, as lower bound of the
+%                   overtaking (left) lane
+% - EndX,EndY -     Point where ends the Safe Zone, as lower bound of the
+%                   overtaking (left) lane
+% - EntryPoint -    Point in the reference map, after the obstacle, where
+%                   the vehicle should return after the overtaking maneuver
+% - detection -     flag that is 1 when obstacle is detected, 0 otherwise
+% - Zone -          flag that tell where the vehicle is with respect to the
+%                   obstacle. Outcomes are:
+%                       - 0 - No obstacle
+%                       - 1 - Well before the obstacle
+%                       - 2 - Before the obstacle, leave the lane
+%                       - 3 - In the Safe Zone, stay in the left lane
+%                       - 4 - After the Safe Zone, come back in your lane
+%                       - 5 - Obstacle passed
+% - IndexObs_old -  Variable used to avoid to search obstacle on map at
+%                   every cycle
+% - DetectionPoint -    Point where to start leave the lane to go in the
+%                       Safe Zone
+%
     % Position of the obstacle
     X_obs = Obstacle(1);
     Y_obs = Obstacle(2);
@@ -17,12 +50,15 @@ function [SafeX,SafeY,EndX,EndY,EntryPoint,detection,Zone,indexObs_old,Detection
     X_act = Actual(1);
     Y_act = Actual(2);
     
+    % Reference speed
+    V_ref = map(1,4);
+    
     % Distance from the obstacle
     distance = sqrt((X_act-X_obs)^2+(Y_act-Y_obs)^2);
 
     indexObs_old = indexObs;
-    % Detect if less than 100 meters
-    if distance < 100
+    % Detect if less than 200 meters
+    if distance < 200
         detection = 1;
  
         % Analyze the map and find the closest point in the map to the
@@ -37,16 +73,28 @@ function [SafeX,SafeY,EndX,EndY,EntryPoint,detection,Zone,indexObs_old,Detection
             [minObs,indexObs] = min(distObsMap);
             indexObs_old = indexObs;
        end
-        % Set the Safe Distance as 1 second before the Obstacle, its end
-        % at 1 second after the obstacle and the reentrance again after 1
-        % second
-        SafeIdx = max(1,round(indexObs-5/Ts));
+       
+        % Set the Safe Distance related with the speed
+        % Safety distance = (V[km/h]/10)^2
+        %
+        % Safe ends 10 meters after the obstacle
+        % Reentrance point is 50 meters after the obstacle
+        % Lane change point is 40 meters before the Safe
+        
+        SafeSteps = V_ref*(3.6/10)^2/Ts;
+        SafeIdx = max(1,round(indexObs-SafeSteps));
         SafePoint = map(SafeIdx,:);
-        EndIdx = min(lengthMap,round(indexObs+5/Ts));
+        
+        EndIdx = min(lengthMap,round(indexObs+10/(V_ref*Ts)));
         EndPoint = map(EndIdx,:);
-        EntryPoint = map(min(lengthMap,round(indexObs+10/Ts)),:);
-        DetectionPoint = map(max(1,round(indexObs-10/Ts)),:);
+        
+        EntryIdx = min(lengthMap,round(indexObs+50/(V_ref*Ts)));
+        EntryPoint = map(EntryIdx,:);
+        
+        DetIdx = max(1,round(indexObs-SafeSteps-40/(V_ref*Ts)));
+        DetectionPoint = map(DetIdx,:);
 
+        % Project Safe zone in the left lane
         SafeX = SafePoint(1) - Lw*sin(SafePoint(3));
         EndX = EndPoint(1) - Lw*sin(EndPoint(3));
 
@@ -57,21 +105,29 @@ function [SafeX,SafeY,EndX,EndY,EntryPoint,detection,Zone,indexObs_old,Detection
 
         % Find the Zone in the nearby of the obstacle
         distActMap = zeros(lengthMap,1);
-        for jj = SafeIdx:EndIdx
+        for jj = DetIdx:EntryIdx
            distActMap(jj) =  sqrt((map(jj,1)-X_act)^2+(map(jj,2)-Y_act)^2);
         end
         
-        [minDist,IdxDist] = min(distActMap(SafeIdx:EndIdx));
-        IdxDist = IdxDist + SafeIdx-1;
-        if IdxDist==SafeIdx
-            % Before the Safe
+        [minDist,IdxDist] = min(distActMap(DetIdx:EntryIdx));
+        IdxDist = IdxDist + DetIdx-1;
+        if IdxDist==DetIdx
+            % Well Before the Safe, stay in your lane
             Zone = 1;
-        elseif IdxDist > SafeIdx && IdxDist < EndIdx
-            % In the Safe Zone
+            detection = 0;
+        elseif IdxDist > DetIdx && IdxDist <= SafeIdx
+            % Before the Safe Zone, change lane
             Zone = 2;
-        else
-            % After the Safe
+        elseif IdxDist > SafeIdx && IdxDist <= EndIdx
+            % In the Safe Zone, stay on left lane
             Zone = 3;
+        elseif IdxDist > EndIdx && IdxDist < EntryIdx
+            % After the Safe, reenter in you lane
+            Zone = 4;
+        else
+            % Obstacle passed, stay in your lane
+            Zone = 5;
+            detection = 0;
         end
             
     else
